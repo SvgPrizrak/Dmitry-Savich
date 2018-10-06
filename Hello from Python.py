5fff19b8079feea9c30b93f74d6311f9fab6d6ce
SvgPrizrak <43899810+SvgPrizrak@users.noreply.github.com>
2018-10-06 20:14:35 +1000

    Update Hello from Python.py

--- Hello from Python.py
+++ Hello from Python.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
-print ("Hello. I am a python program.")
-name = input("What is your name?")
-print ("Hello there, " + name + "!")
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return ("Hello from Python!")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
